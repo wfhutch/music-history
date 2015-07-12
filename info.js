8e454d5bc58f90467dcba423ca5059d6a8d31cef
Wayne Hutchinson <wfh2727@gmail.com>
2015-07-11 19:06:42 -0500

    edited index and added info.js file

---
+++ info.js
@@ -0,0 +1,15 @@
+var artist = ["Beck", "Pink Floyd", "Coldplay", "NEEDTOBREATHE", "John Hiatt"];
+
+var album =["Odelay", "Morning Phase", "Dark Side Of The Moon", "The Wall", "Parachutes", "A Rush Of Blood To The Head", "The Heat", "The Outsiders", "Bring The Family" "Slow Turning"];
+
+var rock = "Rock"
+
+var pop = "Pop";
+
+var blues = "Blues";
+
+var classical = "Classical";
+
+var jazz = "Jazz";
+
+var hipHop = "Hip Hop";
