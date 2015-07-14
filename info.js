c5ed73c108401d439bff46bd223223ae7d97a504
Wayne Hutchinson <wfh2727@gmail.com>
2015-07-14 17:43:33 -0500

    added js file

--- info.js
+++ info.js
@@ -1,25 +1,17 @@
-var artist = ["Beck", "Pink Floyd", "Coldplay", "NEEDTOBREATHE", "John Hiatt"];
+     var songs = [];
 
-var album =["Odelay", "Morning Phase", "Dark Side Of The Moon", "The Wall", "Parachutes", "A Rush Of Blood To The Head", "The Heat", "The Outsiders", "Bring The Family" "Slow Turning"];
-
-var beckSongsOdelay = ["Where It's At", "Hot Wax"];
-
-var beckSongsMP = ["Morning", "Waking Light"];
-
-var pkSongsDark = ["Breathe", "Time"];
-
-var pkSongsWall = ["Hey You", "Comfortably Numb"];
-
-var cpSongsPara = ["Trouble", "Everything's Not Lost"];
-
-var cpSongsRush = ["A Rush Of Blood To The Head", "Amsterdam"];
-
-var ntbSongsHeat = ["The Heat", "Washed By The Water"];
-
-var ntbSongsOut = ["These Hard Times", "Something Beautiful"];
-
-var jhSongsBring = ["Alone In The Dark", "Have A Little Faith In Me"];
-
-var jhSongsSlow = ["Icy Blue Heart", "Feels Like Raing"];
-
-var genre = ["Rock", "Pop", "Blues", "Classical", "Jazz", "Hip Hop"];
+      songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+      songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+      songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+      songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+      songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+      
+      var songElement=document.getElementById("songinfo");
+      songs.push("Waking Light - by Beck on the album Morning Phase");
+      songs.unshift("Washed By The Water - by NEEDTOBREATHE on the album The Heat");
+      for (var i = 0; i < songs.length; i++) {
+        songs[i] = songs[i].replace(">", "-");
+        songs[i] = songs[i].replace(/[*@(!]/g, "");
+        songElement.innerHTML += songs[i] + "<br>";
+      }
+      
