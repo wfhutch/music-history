3b6caef2ab1cf29f024102105b357402052129cc
Wayne Hutchinson <wfh2727@gmail.com>
2015-07-15 17:03:08 -0500

    added styling to song list

--- info.js
+++ info.js
@@ -6,12 +6,14 @@
       songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
       songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
       
-      var songElement=document.getElementById("songinfo");
+      var songElement=document.getElementById("content");
       songs.push("Waking Light - by Beck on the album Morning Phase");
       songs.unshift("Washed By The Water - by NEEDTOBREATHE on the album The Heat");
+      songs.push("There's No Way Out Of Here - By David Gilmour on the album David Gilmour")
+      songs.pop("There's No Way Out Of Here - By David Gilmour on the album David Gilmour")
       for (var i = 0; i < songs.length; i++) {
         songs[i] = songs[i].replace(">", "-");
         songs[i] = songs[i].replace(/[*@(!]/g, "");
-        songElement.innerHTML += songs[i] + "<br>";
+        songElement.innerHTML += '<section class="songs">' + songs[i] + '<section>';
       }
       
