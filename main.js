a9e3df90e5547ae86010ab8b780e3452fe6c2598
Wayne Hutchinson <wfh2727@gmail.com>
2016-01-20 23:03:02 -0600

    Got delete functionality working. Changed button colors

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-     // var songs = [];
-
-     //  songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-     //  songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-     //  songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-     //  songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-     //  songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-      
-     //  var songElement=document.getElementById("content");
-     //  songs.push("Waking Light - by Beck on the album Morning Phase");
-     //  songs.unshift("Washed By The Water - by NEEDTOBREATHE on the album The Heat");
-     //  songs.push("There's No Way Out Of Here - By David Gilmour on the album David Gilmour")
-     //  songs.pop("There's No Way Out Of Here - By David Gilmour on the album David Gilmour")
-     //  for (var i = 0; i < songs.length; i++) {
-     //    songs[i] = songs[i].replace(">", "-");
-     //    songs[i] = songs[i].replace(/[*@(!]/g, "");
-     //    songElement.innerHTML += '<section class="songs">' + songs[i] + '<section>';
-     //  }
-
 
 $(document).ready(function() {
   
@@ -24,56 +5,62 @@ $(document).ready(function() {
      url: "songs.json"
    }).done(function(data) {
    
-   var contentEl = $("#content");
+     var contentEl = $("#content");
 
-   for (var i = 0; i < data.songs.length; i++) {
-     var songData = "";
-     var currentSong = data.songs[i];
-     songData += "<div class='songs'>";
-     songData += "<h2>" + currentSong.name + "</h2>";
-     songData += "</div>"
-     songData += "<div class='songs'>";
-     songData += "by " + currentSong.artist;
-     songData += " on the album " + currentSong.album;    
-     contentEl.append(songData);
-     $("#content").append("<button type='button' id='deleteButton'>Delete</button>");
+     for (var i = 0; i < data.songs.length; i++) {
+       var songData = "";
+       var currentSong = data.songs[i];
+       songData += "<section class='song-div'>";
+       songData += "<div class='songs'>";
+       songData += "<h2>" + currentSong.name + "</h2>";
+       songData += "<p class='song-info'>";
+       songData += "by " + currentSong.artist;
+       songData += " on the album " + currentSong.album + "</p>";
+       songData += "</div>";
+       songData += "<button type='button' class='deleteButton'>Delete</button>";
+       songData += "</section>";
+       contentEl.append(songData);
+     }
 
-
-   }
-
-    $("#deleteButton").click("this.songData", function() {
-        alert("goodbye");
-    })
+     $("body").on("click", ".deleteButton", function(e) {
+        $(this).closest(".song-div").remove();
+     });
 
    })
 
-   $("#content").append("<button type='button' id='moreButton'>More</button>");
+   $("#content").append("<button type='button' class='moreButton'>More Songs</button>");
 
-   $("#moreButton").on("click", function() {
+   $(".moreButton").on("click", function() {
       // alert("hello");
-
     $.ajax({
      url: "songs2.json"
-   }).done(function(data) {
+    }).done(function(data) {
    
-   var contentEl2 = $("#content");
+     var contentEl2 = $("#content");
 
-   for (var i = 0; i < data.songs.length; i++) {
-     var songData2 = "";
-     var currentSong2 = data.songs[i];
-     songData2 += "<div class='songs'>";
-     songData2 += "<h2>" + currentSong2.name + "</h2>";
-     songData2 += "</div>"
-     songData2 += "<div class='songs'>";
-     songData2 += "by " + currentSong2.artist;
-     songData2 += " on the album " + currentSong2.album;    
-     contentEl2.append(songData2);
-     $("#content").append("<button id='deleteButton'>Delete</button>");
-   }
-   $("#content").append("<div id='moreDiv'><button id='moreButton' class='btn btn-success'>More</button>");
+     for (var i = 0; i < data.songs.length; i++) {
+       var songData2 = "";
+       var currentSong2 = data.songs[i];
+       songData2 += "<section class='song-div'>";
+       songData2 += "<div class='songs'>";
+       songData2 += "<h2>" + currentSong2.name + "</h2>";
+       songData2 += "<p class='song-info'>";
+       songData2 += "by " + currentSong2.artist;
+       songData2 += " on the album " + currentSong2.album + "</p>";
+       songData2 += "</div>";
+       songData2 += "<button type='button' class='deleteButton'>Delete</button>";
+       songData2 += "</section>";
+       contentEl2.append(songData2);
 
-   })    
-      
+     }
+
+     $("body").on("click", ".deleteButton", function(e) {
+        $(this).closest(".song-div").remove();
+     });
+
+    }) 
+
+    $(".moreButton").css("display", "none");  
    })
 });
 
